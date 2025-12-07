80eb1564a10740cb7aa8157f3f82783a769485d1
riteshdanuwar <danuwarritesh7>
2025-12-07 07:45:08 +0545

    feat:js basic day 2

---
+++ day2.js
@@ -0,0 +1,33 @@
+// const collegeName = ["abc", "xyz", "pqr"];
+// const name = {
+//   id: 1,
+//   firstname: "abc",
+//   colleges: collegeName,
+// };
+// console.log(collegeName[2]);
+// console.log(name.firstname[2]);
+
+// const students = ["John", "Jane", "Jim", "Jill"];
+// console.log(students.length);
+
+// const collegeDetails = {
+//   name: "techspire",
+//   section: 20,
+//   students: students,
+//   award:null,
+// };
+// console.log(collegeDetails.students[3]);
+// console.log(Object.keys(collegeDetails)[3]);
+
+getStudentName();
+function getStudentName() {
+  console.log("i am normal func");
+}
+
+const getCollegeName = () => {
+  console.log("i am arrow func");
+};
+getCollegeName();
+
+// console.log(abc);
+// var abc="hello";
