73746be08b30cc343728e739cd5f796c7549b695
riteshdanuwar <danuwarritesh7>
2025-12-29 08:23:49 +0545

    feat:user admin

--- day6V2.js
+++ day6V2.js
@@ -1,35 +1,57 @@
 const apiResponse = {
   data: [
-    { 
-        id: 1, 
-        name: "Aurum", 
-        isAdmin: false, 
-        hasUserManagementAccess: false },
-    { 
-        id: 2, 
-        name: "Ritesh", 
-        isAdmin: true, 
-        hasUserManagementAccess: true },
-    { 
-        id: 3, 
-        name: "Aman", 
-        isAdmin: true, 
-        hasUserManagementAccess: false },
+    {
+      id: 1,
+      name: "Aurum",
+      isAdmin: false,
+      hasUserManagementAccess: false,
+    },
+    {
+      id: 2,
+      name: "Ritesh",
+      isAdmin: true,
+      hasUserManagementAccess: true,
+    },
+    {
+      id: 3,
+      name: "Aman",
+      isAdmin: true,
+      hasUserManagementAccess: false,
+    },
   ],
   responseCode: 201,
   responseText: "User fetched successfully",
 };
-const getAdminNames = (response) => {
-  const adminNames = [];
 
-  response.data.forEach(user => {
-    if (user.isAdmin) {
-      adminNames.push(user.name);
+const giveUserMnAcccess = () => {
+  const users = apiResponse.data;
+
+  const updatedUsers = users.map((user) => {
+    if (user.isAdmin && !user.hasUserManagementAccess) {
+      return {
+        ...user,
+        hasUserManagementAccess: true,
+      };
+    } else {
+      return user;
     }
   });
 
-  return adminNames;
+  return updatedUsers;
 };
+console.log(giveUserMnAcccess());
 
-const adminNames = getAdminNames(apiResponse);
-console.log(adminNames);
+// const getAdminNames = (response) => {
+//   const adminNames = [];
+
+//   for (const user of response.data) {
+//     if (user.isAdmin) {
+//       adminNames.push(user.name);
+//     }
+//   }
+
+//   return adminNames;
+// };
+
+// const adminNames = getAdminNames(apiResponse);
+// console.log(adminNames);
